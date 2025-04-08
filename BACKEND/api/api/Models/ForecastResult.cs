d56687e4de8bfd580d28fedb0609e8f18e16c7a0
aron2004HU <sariaron@outlook.com>
2025-04-08 17:03:27 +0200

    ForecastResult Models

---
+++ BACKEND/api/api/Models/ForecastResult.cs
@@ -0,0 +1,9 @@
+﻿namespace api.Models
+{
+    public class ForecastResult
+    {
+        public float Temperature { get; set; }
+        public WeatherType Type { get; set; }
+        public float WindSpeed { get; set; }
+    }
+}
