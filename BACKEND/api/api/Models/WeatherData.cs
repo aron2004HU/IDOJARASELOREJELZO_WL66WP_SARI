3fc2de766a67eb6dcc578bfa46c885e394ecfdc6
aron2004HU <sariaron@outlook.com>
2025-04-08 16:57:31 +0200

    WeatherData Models

---
+++ BACKEND/api/api/Models/WeatherData.cs
@@ -0,0 +1,15 @@
+﻿namespace api.Models
+{
+    public enum WeatherType
+    {
+        Napos,
+        Borult,
+        Esos
+    }
+    public class WeatherData
+    {
+        public float Temperature { get; set; }
+        public WeatherType Type { get; set; }
+        public float WindSpeed { get; set; }
+    }
+}
