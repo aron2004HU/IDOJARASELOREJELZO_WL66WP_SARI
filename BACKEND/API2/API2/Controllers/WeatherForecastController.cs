2570885d595f80c2ffa8e7ffd6c03b6eb70c5050
aron2004HU <sariaron@outlook.com>
2025-04-09 15:46:07 +0200

    Cotroller "ImproveWeather" method added

--- BACKEND/API2/API2/Controllers/WeatherForecastController.cs
+++ BACKEND/API2/API2/Controllers/WeatherForecastController.cs
@@ -1,9 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using API2.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API2.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        private static Random _random = new Random();
 
+        private WeatherType ImproveWeather(WeatherType current)
+        {
+            switch (current)
+            {
+                case WeatherType.Esos:
+                    return WeatherType.Borult;
+                case WeatherType.Borult:
+                    return WeatherType.Napos;
+                case WeatherType.Napos:
+                    return WeatherType.Napos;
+                case WeatherType.Havas:
+                    return WeatherType.Borult;
+                default:
+                    return current;
+            }
+        }
     }
 }
