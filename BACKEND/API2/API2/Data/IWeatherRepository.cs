4f64a7aec057ae20de5fac1055f5dd9cc0377489
aron2004HU <sariaron@outlook.com>
2025-04-09 14:44:21 +0200

    WeatherRepository + Interface

---
+++ BACKEND/API2/API2/Data/IWeatherRepository.cs
@@ -0,0 +1,11 @@
+﻿using API2.Models;
+
+namespace API2.Data
+{
+    public interface IWeatherRepository
+    {
+        void AddWeatherData(WeatherData weather);
+        List<WeatherData> GetAllWeatherData();
+        List<WeatherData> GetLastNWeatherData(int n);
+    }
+}
---
+++ BACKEND/API2/API2/Data/WeatherRepository.cs
@@ -0,0 +1,23 @@
+﻿using API2.Models;
+
+namespace API2.Data
+{
+    public class WeatherRepository : IWeatherRepository
+    {
+        private readonly List<WeatherData> _weatherDataList = new List<WeatherData>();
+        public void AddWeatherData(WeatherData weather)
+        {
+            _weatherDataList.Add(weather);
+        }
+
+        public List<WeatherData> GetAllWeatherData()
+        {
+            return _weatherDataList;
+        }
+
+        public List<WeatherData> GetLastNWeatherData(int n)
+        {
+            return _weatherDataList.Skip(_weatherDataList.Count - n < 0 ? 0 : _weatherDataList.Count - n).ToList();
+        }
+    }
+}
