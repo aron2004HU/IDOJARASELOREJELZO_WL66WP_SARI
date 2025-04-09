ab15b237caa60fb7fb4aa4ea48e5e6979889c6cc
aron2004HU <sariaron@outlook.com>
2025-04-09 15:09:04 +0200

    Program.cs updated

--- BACKEND/API2/API2/Program.cs
+++ BACKEND/API2/API2/Program.cs
@@ -1,6 +1,48 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using API2.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Szolgáltatások regisztrálása
+builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
+// IWeatherDataRepository regisztrálása a WeatherDataRepository implementációval
+builder.Services.AddSingleton<IWeatherRepository, WeatherRepository>();
+
+// Opcionális: Swagger támogatás fejlesztéshez
+//builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+app.UseRouting();
+
+app.MapControllerRoute(
+  name: "default",
+  pattern: "{controller}/{action=Index}/{id?}");
 
 app.MapGet("/", () => "Hello World!");
+
+// Fejleszt?i környezet esetén engedélyezzük a fejleszt?i hibakezelést és a Swagger UI-t
+if (app.Environment.IsDevelopment())
+{
+    //app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+//app.UseHttpsRedirection();
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+app.UseCors(x => x
+     .AllowCredentials()
+     .AllowAnyMethod()
+     .AllowAnyHeader()
+     .WithOrigins("http://127.0.0.1:5500"));
 
 app.Run();
