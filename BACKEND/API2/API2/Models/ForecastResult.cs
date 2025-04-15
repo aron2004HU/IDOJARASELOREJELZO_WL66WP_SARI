2c9c9c6e471a5249a3f51039f9e32055923d77ed
aron2004HU <sariaron@outlook.com>
2025-04-15 15:22:16 +0200

    ForecastResult removed from Models (unused)

--- BACKEND/API2/API2/Models/ForecastResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace API2.Models
-{
-    public class ForecastResult
-    {
-        public double Temperature { get; set; }
-        public WeatherType Type { get; set; }
-        public double WindSpeed { get; set; }
-    }
-}
